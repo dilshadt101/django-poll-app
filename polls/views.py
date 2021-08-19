537f366ccb53d620566e97a5ddaf773d6d8b2bb3
dilshadt101 <dilshadt101@gmail.com>
2021-08-19 22:55:20 +0530

    detailview test added

--- polls/views.py
+++ polls/views.py
@@ -24,6 +24,11 @@ class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
 
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 class ResultsView(generic.DetailView):
     model = Question
@@ -47,3 +52,6 @@ def vote(request, question_id):
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+
+
